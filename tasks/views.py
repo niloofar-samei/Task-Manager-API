0eb1e9c1342e8beb6a73f48e4ec39d4f068c140b
niloofar-samei <niloofar.samei@gmail.com>
2025-08-03 12:41:00 +0100

    Update class TaskListCreateView to make it secure.

--- tasks/views.py
+++ tasks/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Task
 from .serializers import TaskSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 class TaskListCreateView(generics.ListCreateAPIView):
-    queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Task.objects.filter(owner=self.rquest.user)
+
+    def preform_create(self, serializer):
+        serializer.save(owner=self.request.user)

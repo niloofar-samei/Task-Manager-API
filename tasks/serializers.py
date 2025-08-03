6ac42d93a26fa5562e0fbdf200448bbd0a2c5162
niloofar-samei <niloofar.samei@gmail.com>
2025-08-03 12:53:39 +0100

    Create Registration Serializer.

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Task
+from django.contrib.auth.models import User
 
 
 class TaskSerializer(serializers.ModelSerializer):
@@ -7,3 +8,19 @@ class TaskSerializer(serializers.ModelSerializer):
         model = Task
         fields = "__all__"
         read_only_fields = ["owner", "created_at"]
+
+
+class UserRegistrationSerializer(serializer.ModelSerializer):
+    # This line protects the password by hiding it from being accidentally returned.
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "password"]
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validate_data["username"],
+            password=validate_data["password"],
+        )
+        return user

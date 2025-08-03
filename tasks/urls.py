1b3526c0a493caaaa6273f0f6eb7d33abe158a3e
niloofar-samei <niloofar.samei@gmail.com>
2025-08-03 15:08:43 +0100

    Add tasks/<int:pk>/ to urls.

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import TaskListCreateView, UserRegistrationView
+from .views import (
+    TaskListCreateView,
+    UserRegistrationView,
+    TaskRetriveUpdateDestroyView,
+)
 
 urlpatterns = [
     path("tasks/", TaskListCreateView.as_view(), name="task-list-create"),
     path("register/", UserRegistrationView.as_view(), name="user-register"),
+    path("tasks/<int:pk>/", TaskRetriveUpdateDestroyView.as_view(), name="task-detail"),
 ]
